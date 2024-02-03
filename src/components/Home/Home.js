142700c328845cec23e237b6a31dfbfe9749a4db
Mansi <107349957+Mannxxx@users.noreply.github.com>
2024-02-03 15:31:59 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> MANSI</strong>
+                <strong className="main-name"> MANSI (aka Manx)</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
